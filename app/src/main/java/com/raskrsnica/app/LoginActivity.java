8119d16beac17455947da9c358a14f2f0917a1ba
Samsung Laboratorija <samsung@appslab.local>
2017-12-11 15:55:03 +0100

    ubacene stranice

---
+++ app/src/main/java/com/raskrsnica/app/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.raskrsnica.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button button = (Button) findViewById(R.id.loginbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //konekt sa bazom
+                //upit da li je username pronadjen
+                //ako nije, onda greska
+                //ako jeste, select password from baza where username == username
+                //uporedjujemo password-e
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/raskrsnica/app/MainActivity.java
+++ app/src/main/java/com/raskrsnica/app/MainActivity.java
@@ -1,7 +1,14 @@
 package com.raskrsnica.app;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bt1=(Button)findViewById(R.id.settingsbutton);
+        Button bt2=(Button)findViewById(R.id.databasebutton);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager=getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                    BlankFragment fragment1 = new BlankFragment();
+                    fragmentTransaction.replace(android.R.id.content, fragment1);
+                fragmentTransaction.commit();
+
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
