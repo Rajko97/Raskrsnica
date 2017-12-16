d3cea48870d2658f3ed306ac90b6be3d444beebf
Milan Rajkovic <rmilanrajkovic@gmail.com>
2017-12-16 16:02:07 +0100

    prepravka za spiner

--- app/src/main/java/com/raskrsnica/app/SettingsFragment.java
+++ app/src/main/java/com/raskrsnica/app/SettingsFragment.java
@@ -36,15 +36,24 @@ public class SettingsFragment extends Fragment {
     String day = dateformat2.format(c.getTime());
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        //Posto niz raskrsnica treba dinamicki da ucitavamo iz baze, ne moze da se cita iz string.xml (tu su konstante)
+        String[] raskrsnice=new String[]{"Raskrsnica1","Raskrsnica2", "Raskrsnica3", "Raskrsnica4"};
+
+        Spinner spinner1=(Spinner) rootView.findViewById(R.id.spinner1);
+        ArrayAdapter<String> adapter1= new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_spinner_item, raskrsnice);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        //
+        /*
         Spinner spinner1=(Spinner) rootView.findViewById(R.id.spinner1);
         ArrayAdapter<CharSequence> adapter1 =ArrayAdapter.createFromResource(rootView.getContext(),R.array.raskrsnice, android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
-
+*/
         Spinner spinner2=(Spinner) rootView.findViewById(R.id.spinner2);
         ArrayAdapter<CharSequence> adapter2 =ArrayAdapter.createFromResource(rootView.getContext(),R.array.pozicije, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -71,6 +80,9 @@ public class SettingsFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getActivity(),CountingActivity.class);
+                Bundle b = new Bundle();
+
+                intent.putExtras(b);
                 startActivity(intent);
             }
         });
