e2b4af54739e7011bc835c3768a3f9972aa88b99
SandraM97 <sandramilano90@gmail.com>
2018-02-02 21:51:09 +0100

    uradjeni tabovi

--- app/src/main/java/com/raskrsnica/app/MainActivity.java
+++ app/src/main/java/com/raskrsnica/app/MainActivity.java
@@ -4,19 +4,29 @@ package com.raskrsnica.app;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
     final static int REQ_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bt1=(Button)findViewById(R.id.settingsbutton);
-        Button bt2=(Button)findViewById(R.id.databasebutton);
+        final LinearLayout bt1=(LinearLayout) findViewById(R.id.settingsbutton);
+        final LinearLayout bt2=(LinearLayout) findViewById(R.id.databasebutton);
+        final TextView tv1=(TextView)findViewById(R.id.settingstext);
+        final TextView tv2=(TextView)findViewById(R.id.bazatext);
+        final ImageView img1=(ImageView)findViewById(R.id.imgSettings);
+        final ImageView img2=(ImageView)findViewById(R.id.imgBaza);
 
 
         android.support.v4.app.Fragment fragment = null;
@@ -35,6 +45,12 @@ public class MainActivity extends AppCompatActivity{
                 android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
                 ft.replace(R.id.content_frame, fragment);
                 ft.commit();
+                tv1.setTextColor(Color.rgb(217,6,71));
+                tv2.setTextColor(Color.rgb(185,185,185));
+                bt1.setBackgroundColor(Color.rgb(255,255,255));
+                bt2.setBackgroundResource(R.drawable.tab2);
+                img1.setImageResource(R.drawable.ic_podesavanja_color);
+                img2.setImageResource(R.drawable.ic_baza_grey);
 
             }
         });
@@ -45,6 +61,12 @@ public class MainActivity extends AppCompatActivity{
                 android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
                 ft.replace(R.id.content_frame, fragment);
                 ft.commit();
+                tv1.setTextColor(Color.rgb(185,185,185));
+                tv2.setTextColor(Color.rgb(217,6,71));
+                bt2.setBackgroundColor(Color.rgb(255,255,255));
+                bt1.setBackgroundResource(R.drawable.tab1);
+                img1.setImageResource(R.drawable.ic_podesavanja_grey);
+                img2.setImageResource(R.drawable.ic_baza_color);
             }
         });
 
