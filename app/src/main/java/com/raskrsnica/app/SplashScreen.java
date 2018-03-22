daf981689228ccbfd25a85b1ccc343b209b4106f
Samsung Laboratorija <samsung@appslab.local>
2018-03-22 10:10:29 +0100

    asd

--- app/src/main/java/com/raskrsnica/app/SplashScreen.java
+++ app/src/main/java/com/raskrsnica/app/SplashScreen.java
@@ -34,7 +34,8 @@ import java.net.URL;
 import java.net.URLConnection;
 
 public class SplashScreen extends AppCompatActivity {
-    private static String URL_ZA_KORISNIKE = "https://api.myjson.com/bins/g2lx9";
+    //private static String URL_ZA_KORISNIKE = "https://api.myjson.com/bins/g2lx9";
+    private static String URL_ZA_KORISNIKE = "http://192.168.0.106:8000/api/allUsers";
     private static String URL_ZA_ZADATKE = "https://api.myjson.com/bins/16f685";
     private ImageView logo,logo2;
     private ProgressBar pb;
